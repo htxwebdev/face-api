468df02bb62aa7cb2f90c6c7de7e774d167b8c16
htxwebdev <htxwebdev@gmail.com>
2020-07-21 22:47:11 -0500

    heroku errors

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res) => { res.json('server is running') })
+
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
@@ -32,6 +34,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000')
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`)
 })
